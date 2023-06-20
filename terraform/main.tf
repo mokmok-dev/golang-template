199c6c04af64c45116cee89c7f2df1d73b7a1d04
Pranc1ngPegasus <temma.fukaya@mokmok.dev>
2023-06-20 15:22:09 +0900

    feat: setup terraform

---
+++ terraform/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">=1.5.0"
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "5.28.0"
+    }
+  }
+
+  cloud {
+    organization = "mokmok"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "golang-template"
+    }
+  }
+}
+
+provider "github" {
+  owner = var.github.owner
+}
