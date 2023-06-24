f56d3be6ab654cd153470f511bbc86f9b4901536
Fukaya Temma <23367170+Pranc1ngPegasus@users.noreply.github.com>
2023-06-24 16:09:58 +0900

    fix: use envconfig instead of viper (#63)
    * fix: use envconfig instead of viper
    
    * chore: set default values

--- domain/configuration/configuration.go
+++ domain/configuration/configuration.go
@@ -9,29 +9,29 @@ type Config struct {
 }
 
 type Log struct {
-	Level string
+	Level string `envconfig:"LOG_LEVEL" default:"info"`
 }
 
 type Server struct {
-	Port string
+	Port string `envconfig:"SERVER_PORT" default:"8080"`
 }
 
 type Database struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Name     string
+	Host     string `envconfig:"DATABASE_HOST" default:"localhost"`
+	Port     string `envconfig:"DATABASE_PORT" default:"5432"`
+	User     string `envconfig:"DATABASE_USER" default:"postgres"`
+	Password string `envconfig:"DATABASE_PASSWORD" default:"postgres"`
+	Name     string `envconfig:"DATABASE_NAME" default:"golang-template"`
 }
 
 type Email struct {
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Sender   string
+	Host     string `envconfig:"EMAIL_HOST" default:"localhsot"`
+	Port     string `envconfig:"EMAIL_PORT" default:"1025"`
+	User     string `envconfig:"EMAIL_USER" default:"golang-template"`
+	Password string `envconfig:"EMAIL_PASSWORD" default:"password"`
+	Sender   string `envconfig:"EMAIL_SENDER" default:"sender@mokmok.dev"`
 }
 
 type GCP struct {
-	ProjectID string
+	ProjectID string `envconfig:"GCP_PROJECT_ID" default:"mokmok-dev"`
 }
